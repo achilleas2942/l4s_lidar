1f1259787103ec75227fcb6a96b3b908eb97c177
Achilleas Santi Seisa <achilleas2942@gmail.com>
2026-01-23 14:40:42 +0100

    Change FRAME_ID to match the demonstrated pointcloud

--- receiver_scripts/pointcloud_receiver.sh
+++ receiver_scripts/pointcloud_receiver.sh
@@ -7,7 +7,7 @@ set -euo pipefail
 
 PORT="${1:-30112}"
 OUTPUT_TOPIC="${2:-/pointcloud_rx}"
-FRAME_ID="${3:-map}"
+FRAME_ID="${3:-husky/os_sensor}"
 
 #####################################
 # Optional environment hook
